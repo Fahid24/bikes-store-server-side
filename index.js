77bccf5d7525db54608e336b846cd7bd12770960
Fahid24 <fahidhasanfuad20018@gmail.com>
2022-05-07 13:26:37 +0600

    added steps

--- index.js
+++ index.js
@@ -1,44 +1,81 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
-
-app.use(cors())
+// middleware
+app.use(cors());
 app.use(express.json());
 
 
-
-
-const uri = `mongodb+srv:${process.env.DB_USER}//dbuser1:${process.env.DB_PASSWORD}@cluster0.21imy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dagwi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+console.log('db connected')
 async function run() {
     try {
+
         await client.connect();
-        const serviceCollection = clint.db('Bike-inventory').collection('bike-collection')
-        app.get('/service', async (req, res) => {
+        const warehouseCollection = client.db('Bike-inventory').collection('bike-collection');
+
+        app.get('/products', async (req, res) => {
             const query = {};
-            const cursor = serviceCollection.find(query);
-            const services = await cursor.toArray();
+            const cursor = warehouseCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+        app.post('/products', async (req, res) => {
+            const query = req.body;
+            const result = await warehouseCollection.insertOne(query);
+            res.send(result)
+        })
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const cursor = warehouseCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
 
-        });
+        app.get('/myProducts', async (req, res) => {
+            const email = req.query.email;
+            const query = { email };
+            const cursor = warehouseCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
 
+        })
+        app.put('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateProduct = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    quantity: updateProduct.quantity,
+                },
+            };
+            const result = await warehouseCollection.updateOne(filter, updateDoc, options);
+            res.send(result)
+
+        })
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await warehouseCollection.deleteOne(query);
+            res.send(result)
+        })
     }
     finally {
 
     }
 }
-run().catch(console.dir);
-
-
-
-
+run().catch(console.dir)
 app.get('/', (req, res) => {
-    res.send('Running my node Curd server')
+    res.send('runing warehouse management server ');
 })
+
 app.listen(port, () => {
-    console.log('CRUD server is eunning')
+    console.log('runing  port : ', port);
 })
