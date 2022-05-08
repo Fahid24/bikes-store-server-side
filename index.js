fa1dec626df0d6b13db8855f94165556f0ec949b
Fahid24 <fahidhasanfuad20018@gmail.com>
2022-05-08 22:01:01 +0600

    added new site

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
-
 app.use(cors());
 app.use(express.json());
 
@@ -65,7 +64,6 @@ async function run() {
         })
     }
     finally {
-
     }
 }
 run().catch(console.dir)
